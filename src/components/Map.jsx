d1503921bee217eed6794de066c7a30090fc60ee
Christian Heinrich <coffeepyros@gmail.com>
2020-11-19 15:42:39 +0100

    mapdata

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,13 +1,13 @@
 import React from "react";
+import { mapData } from "../mapData";
 import Cell from "./Cell";
 
 export default function Map() {
-  let array = [1, 2, 3];
   return (
     <div>
       <h2>Map</h2>
-      {array.map((cellID) => (
-        <Cell id={cellID} />
+      {mapData.map((col, index) => (
+        <Cell key={index} id={col.col_ID} />
       ))}
     </div>
   );
---
+++ src/mapData.js
@@ -0,0 +1,224 @@
+export const mapData = [
+  {
+    col_ID: -4,
+    data: [
+      { x: -4, y: -3, label: "", terrain: "", notes: "" },
+      { x: -4, y: -2, label: "Sumpf (Untote)", terrain: "swamp", notes: "" },
+      {
+        x: -4,
+        y: -1,
+        label: "Zentauren-<br>Paar",
+        terrain: "steppe",
+        notes:
+          "Alter Zentauren-Lehrer und junge Zentauren-Schülerin (hat für ihren Meister übersetzt). Lehrer meinte die Jugendlichen aus Mythea hätte hier im Zentaurengebiet nichts verloren.",
+      },
+      {
+        x: -4,
+        y: 0,
+        label: "Wasserfall",
+        terrain: "steppe",
+        notes: "Löcher im Boden -> Grabewürmer",
+      },
+      { x: -4, y: 1, label: "", terrain: "", notes: "" },
+      { x: -4, y: 2, label: "", terrain: "", notes: "" },
+      { x: -4, y: 3, label: "", terrain: "", notes: "" },
+    ],
+  },
+  {
+    col_ID: -3,
+    data: [
+      {
+        x: -3,
+        y: -3,
+        label: "Steinwald (Geister),<br/>Riss",
+        terrain: "forest",
+        notes: "",
+      },
+      {
+        x: -3,
+        y: -2,
+        label: "<em>Sova</em>",
+        terrain: "steppe",
+        notes:
+          "Insel im See. Elementarrätsel. Ufer eher steinig/Bruchschiefer. Ereshki von Sova --Halbelfische Druidin + neugieriger, sich merkwürdig verhaltender, Tiger. Thereos hat hier seinen Fokus gebaut.",
+      },
+      { x: -3, y: -1, label: "", terrain: "", notes: "" },
+      { x: -3, y: 0, label: "Arena", terrain: "steppe", notes: "" },
+      { x: -3, y: 1, label: "", terrain: "", notes: "" },
+      {
+        x: -3,
+        y: 2,
+        label: "Path (Flooded)",
+        terrain: "",
+        notes: "Path,\nAncient, Barren, Flooded\nCorrupted by Dark Magic",
+      },
+      {
+        x: -3,
+        y: 3,
+        label: "Tränensee",
+        terrain: "water",
+        notes: "Salzwasser, kein erkennbarer Zufluss",
+      },
+    ],
+  },
+  {
+    col_ID: -2,
+    data: [
+      { x: -2, y: -3, label: "", terrain: "", notes: "" },
+      { x: -2, y: -2, label: "", terrain: "", notes: "" },
+      { x: -2, y: -1, label: "", terrain: "", notes: "" },
+      { x: -2, y: 0, label: "", terrain: "", notes: "" },
+      { x: -2, y: 1, label: "", terrain: "hills", notes: "" },
+      { x: -2, y: 2, label: "", terrain: "forest", notes: "" },
+      {
+        x: -2,
+        y: 3,
+        label: "<em>Pira</em>",
+        terrain: "mountains",
+        notes: "Siedlung im Gebirge, hauptsächlich mit Erzabbau beschäftigt.",
+      },
+    ],
+  },
+  {
+    col_ID: -1,
+    data: [
+      { x: -1, y: -3, label: "", terrain: "", notes: "" },
+      { x: -1, y: -2, label: "", terrain: "", notes: "" },
+      { x: -1, y: -1, label: "", terrain: "", notes: "" },
+      { x: -1, y: 0, label: "", terrain: "", notes: "" },
+      {
+        x: -1,
+        y: 1,
+        label: "Torfabbau, Frostmoor",
+        terrain: "grass",
+        notes: "",
+      },
+      {
+        x: -1,
+        y: 2,
+        label: "<em>Phandalin</em>",
+        terrain: "forest",
+        notes: "",
+      },
+      { x: -1, y: 3, label: "", terrain: "", notes: "" },
+    ],
+  },
+  {
+    col_ID: 0,
+    data: [
+      { x: 0, y: -3, label: "", terrain: "", notes: "" },
+      { x: 0, y: -2, label: "", terrain: "", notes: "" },
+      { x: 0, y: -1, label: "", terrain: "", notes: "" },
+      {
+        x: 0,
+        y: 0,
+        label: "<em>Mythea</em>",
+        terrain: "city",
+        notes:
+          "Siedlung am ehemaligen Waldrand, hauptsächlich mit Holzfällen beschäftigt. \nIn den letzten Jahrzehnten wurde die Waldgrenze immer weiter nach Süden zurück gedrängt. Weizenfelder.",
+      },
+      { x: 0, y: 1, label: "Köhler, Sägemühle", terrain: "grass", notes: "" },
+      { x: 0, y: 2, label: "Frostwald", terrain: "forest", notes: "" },
+      { x: 0, y: 3, label: "", terrain: "", notes: "" },
+    ],
+  },
+  {
+    col_ID: 1,
+    data: [
+      { x: 1, y: -3, label: "", terrain: "", notes: "" },
+      { x: 1, y: -2, label: "", terrain: "", notes: "" },
+      {
+        x: 1,
+        y: -1,
+        label: "The Wall",
+        terrain: "city",
+        notes:
+          "* Ruined: die alte Siedlung, verbrannt - markiert die Südgrenze des Zentaurengebiets\n* Dense\n* Inaccessible\n\n- alter Palisadenzaun (vom Erstaußenposten)\n- Reste der Siedlung wurden als Grenzmarkierung benutzt - ein Holzpfahl / Dachbalken / Wandbrett aller ca. 10m?",
+      },
+      {
+        x: 1,
+        y: 0,
+        label: "Obst- und Olivenhaine",
+        terrain: "grass",
+        notes:
+          "Altes Ehepaar, das die Olivenhaine bewirtschaftet. Hatten nach Hilfe bei der Olivenernte gefragt.",
+      },
+      {
+        x: 1,
+        y: 1,
+        label: "Holzfäller-Lager, Riss",
+        terrain: "forest",
+        notes: "",
+      },
+      { x: 1, y: 2, label: "", terrain: "", notes: "" },
+      { x: 1, y: 3, label: "", terrain: "", notes: "" },
+    ],
+  },
+  {
+    col_ID: 2,
+    data: [
+      { x: 2, y: -3, label: "", terrain: "", notes: "" },
+      { x: 2, y: -2, label: "", terrain: "", notes: "" },
+      { x: 2, y: -1, label: "", terrain: "", notes: "Overlaytest." },
+      { x: 2, y: 0, label: "", terrain: "", notes: "" },
+      { x: 2, y: 1, label: "", terrain: "", notes: "" },
+      { x: 2, y: 2, label: "", terrain: "", notes: "" },
+      { x: 2, y: 3, label: "", terrain: "", notes: "" },
+    ],
+  },
+  {
+    col_ID: 3,
+    data: [
+      { x: 3, y: -3, label: "", terrain: "", notes: "" },
+      { x: 3, y: -2, label: "", terrain: "", notes: "" },
+      { x: 3, y: -1, label: "", terrain: "", notes: "" },
+      { x: 3, y: 0, label: "", terrain: "", notes: "" },
+      { x: 3, y: 1, label: "", terrain: "", notes: "" },
+      { x: 3, y: 2, label: "", terrain: "", notes: "" },
+      { x: 3, y: 3, label: "", terrain: "", notes: "" },
+    ],
+  },
+  {
+    col_ID: 4,
+    data: [
+      { x: 4, y: -3, label: "", terrain: "", notes: "" },
+      { x: 4, y: -2, label: "", terrain: "", notes: "" },
+      { x: 4, y: -1, label: "", terrain: "", notes: "" },
+      { x: 4, y: 0, label: "", terrain: "", notes: "" },
+      { x: 4, y: 1, label: "", terrain: "", notes: "" },
+      { x: 4, y: 2, label: "", terrain: "", notes: "" },
+      { x: 4, y: 3, label: "", terrain: "", notes: "" },
+    ],
+  },
+  {
+    col_ID: 5,
+    data: [
+      {
+        x: 5,
+        y: -3,
+        label: "<em>Argos</em>",
+        terrain: "capitol",
+        notes:
+          "Hauptstadt des lokalen Gebiets. Tempel von Sol: Ausbildung von Paladinen (Sarthak, Helias). Tempel von Luna: steht im Kontakt mit ? (Calliopés Mutter), haben Observatorin Kira geschickt. Manusthenes Mutter Doria kommt von hier aus einer wohlhabenden Händlerfamilie.",
+      },
+      { x: 5, y: -2, label: "", terrain: "", notes: "" },
+      { x: 5, y: -1, label: "", terrain: "", notes: "" },
+      { x: 5, y: 0, label: "", terrain: "", notes: "" },
+      { x: 5, y: 1, label: "", terrain: "", notes: "" },
+      { x: 5, y: 2, label: "", terrain: "", notes: "" },
+      { x: 5, y: 3, label: "", terrain: "", notes: "" },
+    ],
+  },
+  {
+    col_ID: 6,
+    data: [
+      { x: 6, y: -3, label: "", terrain: "", notes: "" },
+      { x: 6, y: -2, label: "", terrain: "", notes: "" },
+      { x: 6, y: -1, label: "", terrain: "", notes: "" },
+      { x: 6, y: 0, label: "", terrain: "", notes: "" },
+      { x: 6, y: 1, label: "", terrain: "", notes: "" },
+      { x: 6, y: 2, label: "", terrain: "", notes: "" },
+      { x: 6, y: 3, label: "", terrain: "", notes: "" },
+    ],
+  },
+];
