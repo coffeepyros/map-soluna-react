8750402101406e95232e3ffa103fb81cf684fefe
coffeepyros <coffeepyros@gmail.com>
2020-12-05 09:11:58 +0100

    fixed overlay, finally!

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Cell from "./Cell";
 import NotesOverlay from "./NotesOverlay";
 import { useSelector } from "react-redux";
 
 export default function Map() {
   const mapData = useSelector((state) => state);
+  const [activeCell, updateActiveCell] = useState(); // use React hook to keep track of the active cell
+  const notesOverlay = document.getElementById("notesOverlay");
+  const overlayOffsetX = 36; // space between mouse curser and overlay (to the right)
+  const overlayOffsetY = -36; // space between mouse curser and start of the overlay (on top)
 
   return (
     <React.Fragment>
-      <figure role="group" id="map">
+      <figure
+        role="group"
+        id="map"
+        onMouseMove={(e) => {
+          if (e.target.classList.contains("hex")) {
+            if (notesOverlay) notesOverlay.style.visibility = "visible";
+            updateActiveCell(e.target); // re-targeting React Hook
+            // if the mouse is over another html element inside a hex cell the event target is this html element,
+            // not the cell itself. So I have to target the parent html element, to hit the cell.
+          } else if (e.target.tagName === "EM" || e.target.tagName === "SPAN") {
+            if (notesOverlay) notesOverlay.style.visibility = "visible";
+            updateActiveCell(e.target.parentElement); // re-targeting React Hook
+          } else {
+            if (notesOverlay) notesOverlay.style.visibility = "hidden";
+          }
+          if (notesOverlay) {
+            notesOverlay.style.top = e.clientY + overlayOffsetY + "px";
+            notesOverlay.style.left = e.clientX + overlayOffsetX + "px";
+          }
+        }}
+      >
+        <NotesOverlay activeCell={activeCell} />
+
         {/* Double map function. First is for the columns of the map (x-direction)
       second is for the individual cells inside each column (y-direction) */}
         {mapData.map((column, colIndex) => (
@@ -27,7 +53,6 @@ export default function Map() {
         ))}
       </figure>
       {/* Overlay for Notes, displayed on hover next to hex cell */}
-      <NotesOverlay frame={document.getElementById("map")} />
     </React.Fragment>
   );
 }
--- src/components/NotesOverlay.jsx
+++ src/components/NotesOverlay.jsx
@@ -4,43 +4,16 @@ import "./NotesOverlay.css";
 
 export default function NotesOverlay(props) {
   const mapData = useSelector((state) => state); // get map data from redux store
-
-  const [activeCell, updateActiveCell] = useState(); // use React hook to keep track of the active cell
   const [label, updateLabel] = useState(); // use React hook to store note details
   const [notes, updateNotes] = useState(); // use React hook to store note details
-
   const notesOverlay = document.getElementById("notesOverlay");
-  const overlayOffsetX = 36; // space between mouse curser and overlay (to the right)
-  const overlayOffsetY = -36; // space between mouse curser and start of the overlay (on top)
-
-  console.log(props.frame);
-
-  // Attaches the overlay position the the mouse curser, so the overlay moves with the mouse
-  // I wanted to constrict the event just to the map itself (<figure id="map">), but I can't
-  // access it here from a child component...
-  document.addEventListener("mousemove", (e) => {
-    if (e.target.classList.contains("hex")) {
-      updateActiveCell(e.target); // re-targeting React Hook
-      // if the mouse is over another html element inside a hex cell the event target is this html element,
-      // not the cell itself. So I have to target the parent html element, to hit the cell.
-    } else if (
-      e.target.classList.contains("em") ||
-      e.target.classList.contains("span")
-    ) {
-      updateActiveCell(e.target.parentElement); // re-targeting React Hook
-    }
-    if (notesOverlay) {
-      notesOverlay.style.top = e.clientY + overlayOffsetY + "px";
-      notesOverlay.style.left = e.clientX + overlayOffsetX + "px";
-    }
-  });
 
   useEffect(() => {
-    if (activeCell) {
+    if (props.activeCell) {
       // the Column and Cell indicies were stored as HTML data attributes
       // during map creating (in Cell.jsx)
-      let colIndex = activeCell.getAttribute("data-col");
-      let cellIndex = activeCell.getAttribute("data-cell");
+      let colIndex = props.activeCell.getAttribute("data-col");
+      let cellIndex = props.activeCell.getAttribute("data-cell");
       let headline = mapData[colIndex].data[cellIndex].label;
       // Clean up map data leftovers from earlier Vanilla JavaScript Map Tool, like HTML tags...
       headline = headline
@@ -54,7 +27,7 @@ export default function NotesOverlay(props) {
       if (content !== "") notesOverlay.classList.remove("hidden");
       else notesOverlay.classList.add("hidden");
     }
-  }, [activeCell, mapData]); // changes the overlay content when the active cell changes,
+  }, [props.activeCell, mapData]); // changes the overlay content when the active cell changes,
   // or when the map data changes
 
   return (
